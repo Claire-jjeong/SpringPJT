1e16edda3f445a3171aa1732c8fffd1a0e376555
Claire-jjeong <kjjung7026@gmail.com>
2022-01-04 21:18:51 +0900

    글 번호 역순 지정

--- SpringMVC01/src/main/java/kr/smhrd/web/BoardController.java
+++ SpringMVC01/src/main/java/kr/smhrd/web/BoardController.java
@@ -7,24 +7,25 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.board.domain.Board;
 import kr.board.mapper.BoardMapper;
 
 @Controller //전처리 하기 위한 표현
-public class BoardController {
+public class BoardController { 
 	
 	@Autowired //메모리에 생성된 객체를 자동으로 연결해줌 
-	private BoardMapper mapper; 
+	private BoardMapper mapper; //new연산자 이용 시 결합도가 높아져 수정에 용이하지 못함 --> xml수정하고 Autowired를 사용 (DI(Dependency Injection) : 의존성 주입)
 
 	//1. 게시판 리스트를 보여달라는 요청을 받아서 DB에서 가져오는 일
 	//HandlerMapping
 	@RequestMapping("/boardList.do")
-	public String boardList(HttpServletRequest request) { //위 요청이 오면 메소드를 실행해라
+	public String boardList(Model model) { //httpservelet -> model : spring 객체 바인딩을 하기 위한 class , 
 		//jsp로 보내기 
 		List<Board> list = mapper.boardList();//boardmapper의 boardList와 연결 -> mapper.xml의 sql문까지 연결
-		request.setAttribute("list", list);
+		model.addAttribute("list", list); //model로 바뀌면서 setattribute-> addattribute
 	
 		//뷰의 논리적인 이름 --ViewResolver--> 뷰의 물리적인 이름(경로)
 		return "boardList"; //보여줄 뷰의 이름 써주기
